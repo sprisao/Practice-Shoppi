c546f158f9157e31ff6b149359caa289e795e897
BruceChoe <whdtmek@gmail.com>
2022-09-07 16:07:53 +0900

    섹션-5: 실습 1. 스플래시 화면 구성 + 앱 호환성

--- app/src/main/java/com/shoppi/kotlin/MainActivity.kt
+++ app/src/main/java/com/shoppi/kotlin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.shoppi.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        Log.d(TAG,"onCreate")
     }
+
+
 }
