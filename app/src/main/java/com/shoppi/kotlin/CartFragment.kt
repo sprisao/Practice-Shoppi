bfacf75962bb221ecc0fa6b67e93821f4bfca50b
BruceChoe <whdtmek@gmail.com>
2022-09-08 09:57:13 +0900

    JetPack 적용, viewmodel dependency 적용

--- app/src/main/java/com/shoppi/kotlin/CartFragment.kt
+++ app/src/main/java/com/shoppi/kotlin/CartFragment.kt
@@ -12,6 +12,6 @@ class CartFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 }
