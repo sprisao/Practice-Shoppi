5a84c614a9e14126e3b841887d8d7849ebda53f4
르네상스맨 <whdtmek@gmail.com>
2022-09-18 21:31:47 +0900

    [섹션 7:Home] RecyclerView - 2. Adapter 구현 with Data Binding

--- app/src/main/java/com/shoppi/kotlin/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/shoppi/kotlin/ui/category/CategoryFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.shoppi.kotlin.R
+import com.shoppi.kotlin.databinding.FragmentCategoryBinding
 import com.shoppi.kotlin.ui.common.ViewModelFactory
 
 class CategoryFragment : Fragment() {
@@ -18,21 +19,25 @@ class CategoryFragment : Fragment() {
         )
     }
 
+    private lateinit var binding: FragmentCategoryBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(
-            R.layout.fragment_category, container, false
-        )
+    ): View {
+        binding =
+            FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val categoryAdapter = CategoryAdapter()
+        binding.rvCategoryList.adapter = categoryAdapter
         viewModel.items.observe(viewLifecycleOwner) {
-            Log.d("CategoryFragment", "items: $it")
+            categoryAdapter.submitList(it)
         }
     }
 }
