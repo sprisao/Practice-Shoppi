7e634df82a9411abf6cc558a7a752868d206201e
brxceo <whdtmek@gmail.com>
2022-09-23 10:00:02 +0900

    [섹션 11] 과제 : 상품 상세 화면 구현 수정 - Data binding layout - 1: 스스로 구현

--- app/src/main/java/com/shoppi/kotlin/network/ApiClient.kt
+++ app/src/main/java/com/shoppi/kotlin/network/ApiClient.kt
@@ -8,7 +8,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ApiClient {
 
--- app/src/main/java/com/shoppi/kotlin/ui/productdetail/ProductDetailFragment.kt
+++ app/src/main/java/com/shoppi/kotlin/ui/productdetail/ProductDetailFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.shoppi.kotlin.R
 import com.shoppi.kotlin.common.KEY_PRODUCT_ID
+import com.shoppi.kotlin.databinding.FragmentProductDetailBinding
 import com.shoppi.kotlin.ui.common.ViewModelFactory
 
 class ProductDetailFragment : Fragment() {
@@ -19,18 +20,25 @@ class ProductDetailFragment : Fragment() {
         )
     }
 
+    private lateinit var binding: FragmentProductDetailBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(
-            R.layout.fragment_product_detail, container, false
-        )
+    ): View {
+        binding =
+            FragmentProductDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.productDetail.observe(viewLifecycleOwner) { productDetail ->
+            Log.e("Data", binding.product.toString())
+            binding.product = productDetail
+        }
 
         val productId = requireArguments().getString(KEY_PRODUCT_ID)
         Log.d("ProductDetailFragment", "productID=$productId")
